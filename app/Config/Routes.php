a2ab36450d221f7750791aef5b73dcec9538781d
RickyPrima30 <113336156+RickyPrima30@users.noreply.github.com>
2023-07-05 17:50:40 +0700

    FIX ROUTES

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -45,6 +45,20 @@ $routes->add('/keranjang', 'TransaksiController::cart_add', ['filter' => 'auth']
 $routes->add('/keranjang/edit', 'TransaksiController::cart_edit', ['filter' => 'auth']);
 $routes->add('/keranjang/delete/(:any)', 'TransaksiController::cart_delete/$1', ['filter' => 'auth']);
 $routes->add('/keranjang/clear', 'TransaksiController::cart_clear', ['filter' => 'auth']);
+$routes->get('/keranjang/getcity', 'TransaksiController::getcity', ['filter' => 'auth']);
+$routes->get('/keranjang/getcost', 'TransaksiController::getcost', ['filter' => 'auth']);
+$routes->add('/keranjang/buy', 'TransaksiController::buy', ['filter' => 'auth']);
+$routes->add('/keranjang/checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+
+$routes->get('/produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->add('/produk', 'ProdukController::create', ['filter' => 'auth']);
+$routes->add('/produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+$routes->get('/produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+$routes->get('/keranjang', 'TransaksiController::cart_show', ['filter' => 'auth']);
+$routes->add('/keranjang', 'TransaksiController::cart_add', ['filter' => 'auth']);
+$routes->add('/keranjang/edit', 'TransaksiController::cart_edit', ['filter' => 'auth']);
+$routes->add('/keranjang/delete/(:any)', 'TransaksiController::cart_delete/$1', ['filter' => 'auth']);
+$routes->add('/keranjang/clear', 'TransaksiController::cart_clear', ['filter' => 'auth']);
 
 $routes->get('/produk', 'ProdukController::index', ['filter' => 'auth']);
 $routes->add('/produk', 'ProdukController::create', ['filter' => 'auth']);
